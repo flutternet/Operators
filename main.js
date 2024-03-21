f14be91eaf51845351f42e75b0709ebcb6885744
flutternet <163728706+flutternet@users.noreply.github.com>
2024-03-21 00:48:36 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+//Airthematical Operators (+,-,*,/,%,**)
+var a = 1;
+var b = 2;
+console.log(a + b);
+var c = 3;
+var d = 4;
+console.log(c - d);
+var e = 5;
+var f = 6;
+console.log(e * f);
+var g = 7;
+var h = 8;
+console.log(g / h);
+var i = 9;
+var j = 10;
+console.log(Math.pow(i, j));
+var k = 11;
+var l = 12;
+console.log(k % l);
---
+++ main.ts
@@ -0,0 +1,19 @@
+//Airthematical Operators (+,-,*,/,%,**)
+let a=1;
+let b=2;
+console.log(a+b);
+let c=3;
+let d=4;
+console.log(c-d);
+let e=5;
+let f=6;
+console.log(e*f);
+let g=7;
+let h=8;
+console.log(g/h);
+let i=9;
+let j=10;
+console.log(i**j);
+let k=11;
+let l=12;
+console.log(k%l);
